8fe07298ed5ac70536aa0e1471ffb19b0dffedee
lollix91 <lollix@live.it>
2022-03-15 00:47:34 +0100

    added arduino

---
+++ ARDUINO_PULSEOXIMETER/ProgettoTesiDottorato/ProgettoTesiDottorato.ino
@@ -0,0 +1,172 @@
+/*
+  Optical SP02 Detection (SPK Algorithm) using the MAX30105 Breakout
+  By: Nathan Seidle @ SparkFun Electronics
+  Date: October 19th, 2016
+  https://github.com/sparkfun/MAX30105_Breakout
+
+  This demo shows heart rate and SPO2 levels.
+
+  It is best to attach the sensor to your finger using a rubber band or other tightening 
+  device. Humans are generally bad at applying constant pressure to a thing. When you 
+  press your finger against the sensor it varies enough to cause the blood in your 
+  finger to flow differently which causes the sensor readings to go wonky.
+
+  This example is based on MAXREFDES117 and RD117_LILYPAD.ino from Maxim. Their example
+  was modified to work with the SparkFun MAX30105 library and to compile under Arduino 1.6.11
+  Please see license file for more info.
+
+  Hardware Connections (Breakoutboard to Arduino):
+  -5V = 5V (3.3V is allowed)
+  -GND = GND
+  -SDA = A4 (or SDA)
+  -SCL = A5 (or SCL)
+  -INT = Not connected
+ 
+  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
+  but it will also run at 3.3V.
+*/
+
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
+
+MAX30105 particleSensor;
+
+#define MAX_BRIGHTNESS 255
+
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+//Arduino Uno doesn't have enough SRAM to store 100 samples of IR led data and red led data in 32-bit format
+//To solve this problem, 16-bit MSB of the sampled data will be truncated. Samples become 16-bit data.
+uint16_t irBuffer[50]; //infrared LED sensor data
+uint16_t redBuffer[50];  //red LED sensor data
+#else
+uint32_t irBuffer[50]; //infrared LED sensor data
+uint32_t redBuffer[50];  //red LED sensor data
+#endif
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+
+void(* resetFunc) (void) = 0; 
+
+void setup()
+{
+  Serial.begin(115200); // initialize serial communication at 115200 bits per second:
+
+  // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.println(F("ERROR: SPO2 Sensor not found."));
+    delay(1000);
+    resetFunc();
+  }
+  else
+    Serial.println("MESSAGE: Sensor found");
+  
+  delay(2000);
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 118; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+}
+
+
+void loop() {
+  sendValues();
+  
+}
+
+
+
+void sendValues() {
+  bufferLength = 100;
+
+    for (byte i = 0; i < bufferLength; i++) {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+/*
+      Serial.println("dati ricevuti");
+      Serial.println(redBuffer[i]);
+      Serial.println(irBuffer[i]);
+*/
+    }
+
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+     
+
+  while (1) {
+
+    for (byte i = 75; i < bufferLength; i++)
+    {
+      redBuffer[i - 75] = redBuffer[i];
+      irBuffer[i - 75] = irBuffer[i];
+    }
+
+
+
+    for (byte i = 75; i < bufferLength; i++) {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+/*
+      Serial.println("dati ricevuti");
+      Serial.println(redBuffer[i]);
+      Serial.println(irBuffer[i]);
+      */
+    }
+
+     
+      
+      //filtering false data - RIMUOVERE IN PRODUZIONE
+      if(spo2 < 81)
+        spo2 = 90;
+
+      if(heartRate > 150)
+        heartRate = 120;
+      
+
+
+      String message = "";
+
+      message = "";
+      message+="MESSAGE:";
+      message+="HR:";
+      message+=heartRate;
+
+      message+=":HRvalid:";
+      message+=validHeartRate;
+
+      message+=":SPO2:";
+      message+=spo2;
+
+      message+=":SPO2Valid:";
+      message+=validSPO2;
+      message+=":";
+
+      Serial.println(message);
+
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
+    
+  }
+}
